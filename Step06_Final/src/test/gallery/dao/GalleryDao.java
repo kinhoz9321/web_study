2915ab90f833ced3c2c2ff106b2b313388ac7435
kimhongju <kinhoz9321@gmail.com>
2021-01-20 00:35:13 +0900

    gallery dao, dto 추가

---
+++ Step06_Final/src/test/gallery/dao/GalleryDao.java
@@ -0,0 +1,308 @@
+package test.gallery.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.cafe.dto.CafeDto;
+import test.gallery.dto.GalleryDto;
+import test.util.DbcpBean;
+
+public class GalleryDao {
+	private static GalleryDao dao;
+	private GalleryDao() {}
+	public static GalleryDao getInstance() {
+		if(dao==null) {
+			dao=new GalleryDao();
+		}
+		return dao;
+	}
+	
+	//인자로 전달되는 이미지 번호에 해당하는 갤러리 정보를 리턴하는 메소드
+	public GalleryDto getData(int num) {
+		GalleryDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT num, writer, caption, imagePath, regdate," + 
+					"		LAG(num, 1, 0) OVER (ORDER BY num DESC) AS prevNum," + 
+					"		LEAD(num, 1, 0) OVER (ORDER BY num DESC) AS nextNum" + 
+					"		FROM board_gallery" + 
+					"		ORDER BY num DESC)" + 
+					"	WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setInt(1, num);
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				dto=new GalleryDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setCaption(rs.getString("caption"));
+				dto.setRegdate(rs.getString("regdate"));
+				dto.setImagePath(rs.getString("imagePath"));
+				dto.setPrevNum(rs.getInt("prevNum"));
+				dto.setNextNum(rs.getInt("nextNum"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return dto;
+	}
+	
+	public int getCount() {
+		int count=0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성 (rownum 최대값, 없으면 0)
+			String sql = "SELECT NVL(MAX(ROWNUM), 0) AS num"
+					+ " FROM board_gallery";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return count;
+	}
+	
+	//업로드 된 사진 하나의 정보를 저장하는 메소드
+	public boolean insert(GalleryDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 insert, update, delete 문 구성
+			String sql = "INSERT INTO board_gallery"
+					+ " (num, writer, caption, imagePath, regdate)"
+					+ " VALUES(board_gallery_seq.NEXTVAL, ?, ?, ?, SYSDATE)";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 바인딩 한다.
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getCaption());
+			pstmt.setString(3, dto.getImagePath());
+			flag = pstmt.executeUpdate();//sql 문 실행하고 변화된 row 갯수 리턴 받기
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//업로드 된 모든 사진의 정보를 리턴하는 메소드
+	public List<GalleryDto> getList(GalleryDto dto){
+		List<GalleryDto> list=new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT result1.*, ROWNUM AS rnum" + 
+					"		FROM" + 
+					"			(SELECT num, writer, caption, imagePath, regdate" + 
+					"			FROM board_gallery" + 
+					"			ORDER BY num DESC) result1)" + 
+					"	WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			while (rs.next()) {
+				GalleryDto dto1=new GalleryDto();
+				dto1.setNum(rs.getInt("num"));
+				dto1.setWriter(rs.getString("writer"));
+				dto1.setCaption(rs.getString("caption"));
+				dto1.setImagePath(rs.getString("imagePath"));
+				dto1.setRegdate(rs.getString("regdate"));
+				list.add(dto1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return list;
+	}
+	
+	//검색조건 작성자인 경우 row의 갯수
+	public int getCountW(GalleryDto dto) {
+		int count=0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT NVL(MAX(ROWNUM),0) AS num"
+					+ " FROM board_gallery"
+					+ " WHERE writer LIKE '%'||?||'%'";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.(키워드는 같지만 다른 필드에 있는 내용을 불러와서 집어넣음)
+			
+			pstmt.setString(1, dto.getWriter());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			if (rs.next()) {
+				count=rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return count;
+	}
+	
+	//작성자 검색인 경우에 글목록 리턴
+	public List<GalleryDto> getListW(GalleryDto dto){
+		List<GalleryDto> list=new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();//DbcpBean()을 설계한다면 여기서 DB를 추출한다. 이거 빼고는 Dao 작성법과 똑같음. 
+			//select 문 작성
+			String sql = "SELECT *" + 
+					"	FROM" + 
+					"		(SELECT result1.*, ROWNUM AS rnum" + 
+					"		FROM" + 
+					"			(SELECT num, writer, caption, imagePath, regdate" + 
+					"			FROM board_gallery" +
+					"         	WHERE writer LIKE '%'||?||'%'" + 
+					"			ORDER BY num DESC) result1)" + 
+					"	WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩 할 게 있으면 여기서 바인딩 한다.
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setInt(2, dto.getStartRowNum());
+			pstmt.setInt(3, dto.getEndRowNum());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet으로부터 data 추출
+			/*
+			 * 로우가 1개면 if문
+			 * 여러개면 while문
+			 */
+			while (rs.next()) {
+				GalleryDto dto1=new GalleryDto();
+				dto1.setNum(rs.getInt("num"));
+				dto1.setWriter(rs.getString("writer"));
+				dto1.setCaption(rs.getString("caption"));
+				dto1.setImagePath(rs.getString("imagePath"));
+				dto1.setRegdate(rs.getString("regdate"));
+				list.add(dto1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return list;
+	}
+}
---
+++ Step06_Final/src/test/gallery/dto/GalleryDto.java
@@ -0,0 +1,110 @@
+package test.gallery.dto;
+
+public class GalleryDto {
+	//필드
+	private int num;
+	private String writer;
+	private String caption;
+	private String imagePath;
+	private String regdate;
+	
+	//페이징 처리 필드
+	private int startRowNum;
+	private int endRowNum;
+	
+	//이전, 다음 버튼 필드
+	private int prevNum;
+	private int nextNum;
+	
+	//디폴트 생성자
+	public GalleryDto() {}
+	
+	//생성자
+	public GalleryDto(int num, String writer, String caption, String imagePath, String regdate, int startRowNum,
+			int endRowNum, int prevNum, int nextNum) {
+		super();
+		this.num = num;
+		this.writer = writer;
+		this.caption = caption;
+		this.imagePath = imagePath;
+		this.regdate = regdate;
+		this.startRowNum = startRowNum;
+		this.endRowNum = endRowNum;
+		this.prevNum = prevNum;
+		this.nextNum = nextNum;
+	}
+
+	//getter setter
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getStartRowNum() {
+		return startRowNum;
+	}
+
+	public void setStartRowNum(int startRowNum) {
+		this.startRowNum = startRowNum;
+	}
+
+	public int getEndRowNum() {
+		return endRowNum;
+	}
+
+	public void setEndRowNum(int endRowNum) {
+		this.endRowNum = endRowNum;
+	}
+
+	public int getPrevNum() {
+		return prevNum;
+	}
+
+	public void setPrevNum(int prevNum) {
+		this.prevNum = prevNum;
+	}
+
+	public int getNextNum() {
+		return nextNum;
+	}
+
+	public void setNextNum(int nextNum) {
+		this.nextNum = nextNum;
+	}
+	
+}
